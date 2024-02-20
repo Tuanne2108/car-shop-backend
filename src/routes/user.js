a31e312a0fdecda2612d68589afeb8fb25f5d028
Tuanne2108 <107233041+Tuanne2108@users.noreply.github.com>
2024-02-20 17:39:15 +0700

    add authentication for update user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const UserController = require("../controllers/UserController");
-const { authenMiddleware, authenUserMiddleware } = require("../middleware/AuthenMiddleware");
+const {
+    authenMiddleware,
+    authenUserMiddleware,
+} = require("../middleware/AuthenMiddleware");
 const router = express.Router();
 
 router.post("/log-in", UserController.logInUser);
 router.post("/log-up", UserController.createUser);
 router.post("/logout", UserController.logOutUser);
-router.put("/update-user/:id", UserController.updateUser);
+router.put("/update-user/:id", authenUserMiddleware, UserController.updateUser);
 router.delete("/delete-user/:id", authenMiddleware, UserController.deleteUser);
 router.get("/get-all-user", authenMiddleware, UserController.getAllUser);
-router.get("/get-user-details/:id",authenUserMiddleware, UserController.getUserDetails);
+router.get(
+    "/get-user-details/:id",
+    authenUserMiddleware,
+    UserController.getUserDetails
+);
 router.post("/refresh-token", UserController.refreshToken);
 module.exports = router;
