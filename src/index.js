0a6e6b8a8ded53c4d63848cd2110af8943ca732a
Tuanne2108 <107233041+Tuanne2108@users.noreply.github.com>
2024-02-21 16:45:34 +0700

    fix bug payloadTooLarge for avatar

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const cookieParser = require('cookie-parser')
 
 app.use(cors())
 app.use(morgan("combined"));
+app.use(express.json({limit: '200mb'}));
+app.use(express.urlencoded({limit: '200mb', extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
